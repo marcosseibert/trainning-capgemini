383d6a2189157ec0ee198b695eb0b510cf3e1bc2
Marcos Seibert dos Santos <marcos_seibert@hotmail.com>
2023-02-28 17:26:19 -0300

    first commit

---
+++ src/main/java/com/capgemini/trainningcapgemini/TrainningCapgeminiApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.trainningcapgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainningCapgeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainningCapgeminiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/capgemini/trainningcapgemini/TrainningCapgeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.trainningcapgemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainningCapgeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
